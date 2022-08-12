988a291952e7103da1a5b5832693aab008c7c9b3
Uris <urialbertogalvafrancisco@gmail.com>
2022-08-12 03:14:55 -0400

    sdk mapbox added and fixing styles (mapcontainer)

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+import 'mapbox-gl/dist/mapbox-gl.css';
+mapboxgl.accessToken = 'pk.eyJ1IjoidXJpNzgwIiwiYSI6ImNrcW9rNWhhNzBsZmMyd3IxOTZvdW12N24ifQ.lYQwBYJF5ieRaKCVYRFphg';
+
+
 
 export const Map = () => {
+    const [map, setMap] = useState();
+    const mapBoxRef= useRef();
+    useEffect(() => {
+        const map = new mapboxgl.Map({
+            container: mapBoxRef.current, // container ID
+            style: 'mapbox://styles/mapbox/streets-v11', // style URL
+            center: [-74.5, 40], // starting position [lng, lat]
+            zoom: 9, // starting zoom
+            projection: 'globe' // display the map as a 3D globe
+        });
+        map.on('style.load', () => {
+            map.setFog({}); // Set the default atmosphere style
+        });
+
+        setMap(map);
+
+    }, [])
+    
   return (
-    <div>Map</div>
+    <div className='mapContainer' ref={mapBoxRef}>Map</div>
   )
 }
